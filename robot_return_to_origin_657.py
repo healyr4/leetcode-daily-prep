609fb271c4eff954d50369e703680723db23e14d
Robert <healyr4@tcd.ie>
2026-06-08 13:39:33 +0100

    feature/robot-return-origin added sample input tests

--- robot_return_to_origin_657.py
+++ robot_return_to_origin_657.py
@@ -12,8 +12,10 @@ if __name__ == "__main__":
     assert solution.judge_circle("LL") is False
     assert solution.judge_circle("UDLR") is True
     assert solution.judge_circle("UU") is False
+    assert solution.judge_circle("UUUUU") is False
+    assert solution.judge_circle("R") is False
 
-    print("All tests passed!")
+    print("All tests have passed")
 
 
 
