bdc22bbdeabc7b3b2b3c13ce87027a6eda964f90
Robert <healyr4@tcd.ie>
2026-06-08 13:45:34 +0100

    feature/robot-return-origin added unit tests for file

---
+++ tests/test_robot_return_to_origin_657.py
@@ -0,0 +1,28 @@
+from robot_return_to_origin_657 import Solution
+
+solution = Solution()
+
+
+def test_returns_true_for_up_down():
+    solution = Solution()
+    assert solution.judge_circle("UD") is True
+
+
+def test_returns_false_for_two_lefts():
+    solution = Solution()
+    assert solution.judge_circle("LL") is False
+
+
+def test_returns_true_for_balanced_all_directions():
+    solution = Solution()
+    assert solution.judge_circle("UDLR") is True
+
+
+def test_returns_false_for_unbalanced_vertical():
+    solution = Solution()
+    assert solution.judge_circle("UUD") is False
+
+
+def test_returns_false_for_unbalanced_horizontal():
+    solution = Solution()
+    assert solution.judge_circle("RRRL") is False
