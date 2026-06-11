bb7a72142d4eccde540ec0ae105cf2c85b7d767b
Robert <healyr4@tcd.ie>
2026-06-11 20:07:53 +0100

    feature/robot-return-origin Added tests

---
+++ src/robot_return_to_origin_657.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+
+class Solution:
+    def judge_circle(self, moves: str) -> bool:
+        counts = Counter(moves)
+        return counts["U"] == counts["D"] and counts["L"] == counts["R"]
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    assert solution.judge_circle("UD") is True
+    assert solution.judge_circle("LL") is False
+    assert solution.judge_circle("UDLR") is True
+    assert solution.judge_circle("UU") is False
+    assert solution.judge_circle("UUUUU") is False
+    assert solution.judge_circle("R") is False
+
+    print("All tests have passed")
