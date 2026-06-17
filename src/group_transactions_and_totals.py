ae11e7cdb19129b00c1aca059e0cc958de13af3a
Robert <healyr4@tcd.ie>
2026-06-17 20:20:56 +0100

    feature/live-coding-practise Using Pydantic and Decimal instead of int

--- src/group_transactions_and_totals.py
+++ src/group_transactions_and_totals.py
@@ -1,47 +1,36 @@
 # Group transaction totals by account
 # Assume input is list of dicts with account_id and amount
-from typing import TypedDict
+from decimal import Decimal
+from pydantic import BaseModel
 
-class Transaction(TypedDict):
+
+class Transaction(BaseModel):
     account_id: str
-    amount: int | str
+    amount: Decimal
 
-# def get_totals_by_account_id(transactions: list[dict[str, int | str]]) -> dict[str, int]:
-#     totals_by_account = {}
-#     for transaction in transactions:
-#         account_id = transaction["account_id"]
-#         amount = transaction["amount"]
-#         if account_id in totals_by_account:
-#             totals_by_account[account_id] += int(amount)
-#         else:
-#             totals_by_account[account_id] = int(amount)
-#     return totals_by_account
 
-def get_totals_by_account_id(transactions: list[Transaction]) -> dict[str, int]:
+def get_totals_by_account_id(transactions: list[Transaction]) -> dict[str, Decimal]:
 
-    totals_by_account = {}
+    totals_by_account: dict[str, Decimal] = {}
     for transaction in transactions:
-        account_id = transaction["account_id"]
-        amount = transaction["amount"]
-        if account_id in totals_by_account:
-            totals_by_account[account_id] += int(amount)
-        else:
-            totals_by_account[account_id] = int(amount)
+        totals_by_account[transaction.account_id] = (
+            totals_by_account.get(transaction.account_id, Decimal(0))
+            + transaction.amount
+        )
     return totals_by_account
 
 
 def main():
-    transactions: list[Transaction] = [{"account_id": "AR101", "amount": 75},
-                    {"account_id": "AR102", "amount": 100},
-                    {"account_id": "AR101", "amount": 20},
-                    {"account_id": "AR102", "amount": 30},
-                    {"account_id": "AR103", "amount": 43}]
-
+    transactions = [
+        Transaction(account_id="AR101", amount=Decimal(75)),
+        Transaction(account_id="AR102", amount=Decimal(100)),
+        Transaction(account_id="AR101", amount=Decimal(20)),
+        Transaction(account_id="AR102", amount=Decimal(30)),
+        Transaction(account_id="AR103", amount=Decimal(43)),
+    ]
     totals = get_totals_by_account_id(transactions)
     print(totals)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
-
-
