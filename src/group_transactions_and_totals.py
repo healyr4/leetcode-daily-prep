775d3cde9e673abebbadf6526ad17bcaeb8cadeb
Robert <healyr4@tcd.ie>
2026-06-17 20:22:42 +0100

    feature/live-coding-practise Using Decimal(0) etc for safety

--- src/group_transactions_and_totals.py
+++ src/group_transactions_and_totals.py
@@ -1,6 +1,5 @@
-# Group transaction totals by account
-# Assume input is list of dicts with account_id and amount
 from decimal import Decimal
+
 from pydantic import BaseModel
 
 
@@ -9,25 +8,31 @@ class Transaction(BaseModel):
     amount: Decimal
 
 
-def get_totals_by_account_id(transactions: list[Transaction]) -> dict[str, Decimal]:
-
+def get_totals_by_account_id(
+    transactions: list[Transaction],
+) -> dict[str, Decimal]:
     totals_by_account: dict[str, Decimal] = {}
+
     for transaction in transactions:
-        totals_by_account[transaction.account_id] = (
-            totals_by_account.get(transaction.account_id, Decimal(0))
+        account_id = transaction.account_id
+
+        totals_by_account[account_id] = (
+            totals_by_account.get(account_id, Decimal("0"))
             + transaction.amount
         )
+
     return totals_by_account
 
 
-def main():
-    transactions = [
-        Transaction(account_id="AR101", amount=Decimal(75)),
-        Transaction(account_id="AR102", amount=Decimal(100)),
-        Transaction(account_id="AR101", amount=Decimal(20)),
-        Transaction(account_id="AR102", amount=Decimal(30)),
-        Transaction(account_id="AR103", amount=Decimal(43)),
+def main() -> None:
+    transactions: list[Transaction] = [
+        Transaction(account_id="AR101", amount=Decimal("75")),
+        Transaction(account_id="AR102", amount=Decimal("100")),
+        Transaction(account_id="AR101", amount=Decimal("20")),
+        Transaction(account_id="AR102", amount=Decimal("30")),
+        Transaction(account_id="AR103", amount=Decimal("43")),
     ]
+
     totals = get_totals_by_account_id(transactions)
     print(totals)
 
