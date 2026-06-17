5fb864455f9d98d00ff626b83d0a6a072a64ff0f
Robert <healyr4@tcd.ie>
2026-06-17 20:07:43 +0100

    feature/live-coding-practise Using TypedDict for static type checking

--- src/group_transactions_and_totals.py
+++ src/group_transactions_and_totals.py
@@ -1,7 +1,24 @@
 # Group transaction totals by account
 # Assume input is list of dicts with account_id and amount
+from typing import TypedDict
 
-def get_totals_by_account_id(transactions: list[dict[str, int | str]]) -> dict[str, int]:
+class Transaction(TypedDict):
+    account_id: str
+    amount: int | str
+
+# def get_totals_by_account_id(transactions: list[dict[str, int | str]]) -> dict[str, int]:
+#     totals_by_account = {}
+#     for transaction in transactions:
+#         account_id = transaction["account_id"]
+#         amount = transaction["amount"]
+#         if account_id in totals_by_account:
+#             totals_by_account[account_id] += int(amount)
+#         else:
+#             totals_by_account[account_id] = int(amount)
+#     return totals_by_account
+
+def get_totals_by_account_id(transactions: list[Transaction]) -> dict[str, int]:
+
     totals_by_account = {}
     for transaction in transactions:
         account_id = transaction["account_id"]
@@ -12,8 +29,9 @@ def get_totals_by_account_id(transactions: list[dict[str, int | str]]) -> dict[s
             totals_by_account[account_id] = int(amount)
     return totals_by_account
 
+
 def main():
-    transactions = [{"account_id": "AR101", "amount": 75},
+    transactions: list[Transaction] = [{"account_id": "AR101", "amount": 75},
                     {"account_id": "AR102", "amount": 100},
                     {"account_id": "AR101", "amount": 20},
                     {"account_id": "AR102", "amount": 30},
