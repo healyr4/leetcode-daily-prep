ae1d8029189337290cbc4dc5860f255ae6e4d97f
Robert <healyr4@tcd.ie>
2026-07-02 15:20:03 +0100

    feature/min_window_to_sort Expanding boundaries to check for ans and checking for already sorted case

--- src/min_window_to_sort.py
+++ src/min_window_to_sort.py
@@ -13,9 +13,22 @@
 
 test_case = [1, 2, 6, 5, 5, 8, 9]
 
-def main():
-    left_bound = 0
-    right_bound = 0
+def expand_bounds(test_case, lb, rb, subarray_min, subarray_max):
+    while (lb >0 and test_case[lb -1] > subarray_min):
+        lb -= 1
+
+    while (rb <len(test_case) - 1 and test_case[rb+1] < subarray_max):
+        rb +=1
+    return lb, rb
+
+def get_min_max_of_subarray(test_case, lb, rb):
+    subarray_min = min(test_case[lb:rb+1])
+    subarray_max = max(test_case[lb:rb+1])
+    return subarray_min, subarray_max
+
+def get_boundaries(test_case):
+    left_bound = None
+    right_bound = None
     for i in range(len(test_case)-1):
         curr_val = test_case[i]
         next_val = test_case[i+1]
@@ -24,12 +37,24 @@ def main():
         else:
             left_bound = i
             break
+    if left_bound is None:
+        return None, None
     for j in range(len(test_case)-1, 0, -1):
         if test_case[j] < test_case[j-1]:
             right_bound = j
             break
 
-    print(left_bound, right_bound)
+    return left_bound, right_bound
+
+def main():
+    left_bound, right_bound = get_boundaries(test_case)
+    if left_bound is None:
+        return [-1, -1]
+    subarray_min, subarray_max = get_min_max_of_subarray(test_case, left_bound, right_bound)
+    left_bound_final, right_bound_final = expand_bounds(test_case, left_bound, right_bound, subarray_min, subarray_max)
+    print([left_bound_final, right_bound_final])
+    return [left_bound_final, right_bound_final]
+
 
 
 
